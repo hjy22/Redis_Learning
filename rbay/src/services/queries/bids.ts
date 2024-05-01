6e546de9bcf8db6be4f01eb0c3997cf26d606edf
Jiayi Huang <huangjiayi@dhcp-v087-073.mobile.uci.edu>
2024-05-01 13:11:02 -0700

    2024.4.30

--- rbay/src/services/queries/bids.ts
+++ rbay/src/services/queries/bids.ts
@@ -1,17 +1,46 @@
 import type { CreateBidAttrs, Bid } from '$services/types';
 import { client } from '$services/redis';
-import { bidHistoryKey } from '$services/keys';
+import { bidHistoryKey, itemsKey,itemsByPriceKey } from '$services/keys';
 import { DateTime } from 'luxon';
-import { serialize } from 'cookie';
+import { getItem } from './items';
+
 
 export const createBid = async (attrs: CreateBidAttrs) => {
-	const serialized = serializeHistory(
-		attrs.amount,
-		attrs.createdAt.toMillis()
-	);
 
-	return client.rPush(bidHistoryKey(attrs.itemId),serialized);
-};
+	return client.executeIsolated(async(isolatedClient)=>{
+		await isolatedClient.watch(itemsKey(attrs.itemId));
+
+		const item =await getItem(attrs.itemId)
+		if (!item){
+			throw new Error('Item does not exist')
+		}
+		if(item.price>=attrs.amount){
+			throw new Error('Bid too low')
+		}
+		if(item.endingAt.diff(DateTime.now()).toMillis()<0){
+			throw new Error('Item closed to binding')
+		}
+
+		const serialized = serializeHistory(
+			attrs.amount,
+			attrs.createdAt.toMillis()
+		);
+
+		return isolatedClient
+			.multi()
+			.rPush(bidHistoryKey(attrs.itemId),serialized)
+			.hSet(itemsKey(item.id),{
+				bids:item.bids+1,
+				price:attrs.amount,
+				highestBidUserIs:attrs.userId
+			})
+			.zAdd(itemsByPriceKey(),{
+				value:item.id,
+				score:attrs.amount
+			})
+			.exec();
+	});
+}
 
 export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {
 	const startIndex = -1*offset-count;
