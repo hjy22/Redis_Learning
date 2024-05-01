1a16864962beff8fbd80a41754896410b3db7833
Jiayi Huang <huangjiayi@dhcp-v087-073.mobile.uci.edu>
2024-05-01 14:08:46 -0700

    2024.5.2

--- rbay/src/services/queries/views.ts
+++ rbay/src/services/queries/views.ts
@@ -1,13 +1,15 @@
 import { client } from '$services/redis';
-import { itemsKey, itemsByViewSkey,itemsViewsKey } from '$services/keys';
+// import { itemsKey, itemsByViewSkey,itemsViewsKey } from '$services/keys';
 
 export const incrementView = async (itemId: string, userId: string) => {
-    const inserted = await client.pfAdd(itemsViewsKey(itemId),userId);
+    //用Lua替换
+    return client.incrementView(itemId,userId)
+    // const inserted = await client.pfAdd(itemsViewsKey(itemId),userId);
 
-    if(inserted){
-        return Promise.all([
-            client.hIncrBy(itemsKey(itemId), 'views', 1),
-            client.zIncrBy(itemsByViewSkey(), 1, itemId)
-        ]);
-    }
+    // if(inserted){
+    //     return Promise.all([
+    //         client.hIncrBy(itemsKey(itemId), 'views', 1),
+    //         client.zIncrBy(itemsByViewSkey(), 1, itemId)
+    //     ]);
+    // }
 };
--- rbay/src/services/redis/client.ts
+++ rbay/src/services/redis/client.ts
@@ -1,12 +1,64 @@
-import { createClient } from 'redis';
+import { itemsByViewSkey, itemsKey, itemsViewsKey } from '$services/keys';
+import { incrementView } from '$services/queries/views';
+import { createClient,defineScript } from 'redis';
+import { isYieldExpression } from 'typescript';
 
 const client = createClient({
 	socket: {
 		host: process.env.REDIS_HOST,
 		port: parseInt(process.env.REDIS_PORT)
 	},
-	password: process.env.REDIS_PW
+	password: process.env.REDIS_PW,
+	scripts:{
+		addOneAndStore:defineScript({
+			NUMBER_OF_KEYS: 1,
+			//SCRIPT - Lua script
+			SCRIPT:`
+				return redis.call('SET', KEY[1],1+tonumber(ARGV[1]))
+			`,
+			transformArguments(key:string, value:number){
+				return [key,value.toString()]
+				// ['book:count','5']
+				// EVALSHA <ID> 1 'books:count' '5'
+			},
+			transformReply(reply:any){
+				return reply
+			}
+		}),
+		incrementView:defineScript({
+			NUMBER_OF_KEYS:3,
+			SCRIPT:`
+				local itemsViewsKey = KEYS[1]
+				local itemsKey = KEYS[2]
+				local itemsByViewsKey = KEYS[3]
+				local itemId = ARGV[1]
+				local userId = ARGV[2]
+
+				local inserted = redis.call('PFADD',itemsViewsKey,userId)
+				if inserted == 1 then
+					redis.call('HINCRBY',itemsKey,'views',1)
+					redis.call('ZINCRBY',itemsByViewsKey,1,itemId)
+				end
+			`,
+			transformArguments(itemId:string,userId:string){
+				return [
+					itemsViewsKey(itemId),
+					itemsKey(itemId),
+					itemsByViewSkey(),
+					itemId,
+					userId	
+				];
+			},
+			transformReply(){}
+		})
+	}
 });
+
+client.on('connect',async()=>{
+	await client.addOneAndStore('books:count',5)
+	const result = await client.get('books:count')
+	console.log(result)
+})
 
 client.on('error', (err) => console.error(err));
 client.connect();
